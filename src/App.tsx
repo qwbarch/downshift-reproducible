47b4da1c098c8ddf0e3fc10fe00623ec82197ec5
qwbarch <qwbarch@gmail.com>
2022-11-24 23:18:55 -0500

    Add example

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useCombobox } from "downshift";
+import { useState } from "react";
 
-function App() {
+const fruits = [
+  "Apple",
+  "Orange",
+  "Pear",
+  "Grapes",
+  "Grapefruit",
+  "Raspberries",
+  "Tangerine",
+  "Papaya",
+  "Pomegranate",
+];
+
+export default function App() {
+  const [items, setItems] = useState(fruits);
+  const {
+    isOpen,
+    getInputProps,
+    getItemProps,
+    getMenuProps,
+    highlightedIndex,
+  } = useCombobox({
+    onInputValueChange({ inputValue }) {
+      setItems(
+        fruits.filter(
+          (fruit) =>
+            !inputValue ||
+            fruit.toLowerCase().includes(inputValue.toLowerCase())
+        )
+      );
+    },
+    items,
+    defaultHighlightedIndex: 0,
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <input {...getInputProps()} />
+      <ul {...getMenuProps()}>
+        {isOpen &&
+          items.map((item, index) => (
+            <li key={`${item}${index}`} {...getItemProps({ item, index })}>
+              <span>
+                {highlightedIndex === index ? "Selected: " : ""}
+                {item}
+              </span>
+            </li>
+          ))}
+      </ul>
+    </>
   );
 }
-
-export default App;
